2c8e2ef26dac14c144af4378568e9dca2590bdfd
Stefan <ste.schaller@gmx.de>
2019-01-30 22:41:59 +0100

    Add licences

--- example/stateful_view_model_example.dart
+++ example/stateful_view_model_example.dart
@@ -2,14 +2,15 @@ import 'package:stateful_view_model/stateful_view_model.dart';
 
 import 'package:meta/meta.dart';
 
-
 abstract class UserService {
   Stream<bool> login(String email, String password);
 }
 
-
+///
+/// The [State] represents the [View].
+/// The [State] need's to implement the [Cloneable] interface!
+///
 class LoginState implements Cloneable<LoginState> {
-
   String email;
   String password;
   bool loginButtonEnabled;
@@ -17,10 +18,9 @@ class LoginState implements Cloneable<LoginState> {
 
   LoginState(
       {@required this.email,
-        @required this.password,
-        @required this.loginButtonEnabled,
-        @required this.isLoading});
-
+      @required this.password,
+      @required this.loginButtonEnabled,
+      @required this.isLoading});
 
   @override
   LoginState copy() => LoginState(
@@ -30,51 +30,41 @@ class LoginState implements Cloneable<LoginState> {
       isLoading: isLoading);
 }
 
-abstract class LoginViewModel extends StatefulViewModel<LoginState> {
-  LoginViewModel(LoginState initialState) : super(initialState);
-
-  void setMail(String emailInput);
-
-  void setPassword(String passwordInput);
-
-  void login();
-}
-
-class LoginViewModelImpl extends LoginViewModel {
+class LoginViewModelImpl extends StatefulViewModel<LoginState> {
   final UserService _userService;
 
   LoginViewModelImpl(this._userService, LoginState initialState)
       : super(initialState);
 
-  @override
   void setMail(String emailInput) {
     LoginState state = getState();
     _updateInput(emailInput, state.password);
   }
 
-  @override
   void setPassword(String passwordInput) {
     LoginState state = getState();
     _updateInput(state.email, passwordInput);
   }
 
-  void _updateInput(String email, String password) {
+  void login() {
+    LoginState state = getState();
 
+    disposeSubscription(
+        _userService.login(state.email, state.password).listen((success) {
+      //Successfully logged in
+    }));
+  }
+
+  // -----
+  // Helper
+  // -----
+
+  void _updateInput(String email, String password) {
     setState((state) {
       state.email = email;
       state.password = password;
       state.loginButtonEnabled = email.isEmpty && password.isEmpty;
       return state;
     });
-  }
-
-  @override
-  void login() {
-    LoginState state = getState();
-
-    disposeSubscription(_userService.login(state.email, state.password).listen((success)
-    {
-      //Successfully logged in
-    }));
   }
 }
